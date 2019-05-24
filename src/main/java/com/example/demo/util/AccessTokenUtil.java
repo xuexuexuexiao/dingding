8c86dc675865d72a577ef554350ab144a6516973
xuexuexuease <2752037870@qq.com>
2019-05-24 14:34:25 +0800

    first commit

---
+++ src/main/java/com/example/demo/util/AccessTokenUtil.java
@@ -0,0 +1,11 @@
+package com.example.demo.util;
+
+/**
+ * @author Huxiaoxue
+ * @version V1.0
+ * @ClassName
+ * @desc: accesstoken
+ * @create: 2019-05-24
+ **/
+public class AccessTokenUtil {
+}
---
+++ src/main/java/com/example/demo/util/HttpHelp.java
@@ -0,0 +1,11 @@
+package com.example.demo.util;
+
+/**
+ * @author Huxiaoxue
+ * @version V1.0
+ * @ClassName
+ * @desc: httpUtil
+ * @create: 2019-05-24
+ **/
+public class HttpHelp {
+}
